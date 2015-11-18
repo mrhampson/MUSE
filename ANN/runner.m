db40739fe45ac043c287818a4d9a6314ca036180
Nick Layton <nllayton@ucdavis.edu>
2015-11-18 14:39:30 -0800

    starting to write the first problem

---
+++ ANN/runner.m
@@ -0,0 +1,14 @@
+% runner: performs all calculations for analysis with Artificial Neural
+% Networks
+
+% perform first problem
+
+% perform second problem
+
+% perform third problem
+
+% perform fourth problem
+
+% perform fifth problem
+
+% perform sixth problem (should we be doing this?)
