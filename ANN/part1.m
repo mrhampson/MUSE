7deb4a7098101e9a1fce04829470c21791db2906
Nick Layton <nllayton@ucdavis.edu>
2015-11-18 17:43:15 -0800

    Refine the documentation for the runner and Part1

--- ANN/part1.m
+++ ANN/part1.m
@@ -1,11 +1,18 @@
 function [ networks ] = part1(x, y)
-% part1(): performs calculations for part1 by
-% 		   constructing and training multiple
-%		   Neural Networks with training data
+% part1(): Performs calculations for part 1 by constructing 
+%		   and training various Neural Networks with various
+%		   learning rates. Training and testing data are 
+%		   extracted from the dataset that is provided: (x, y)
+
 	disp('****************************************')
 	disp('*** PART 1 *****************************')
 
-	% TODO: construct training and validation datasets from original
+	% Extract the training data from the entire dataset (x, y) by selecting
+	% the genre of the song as the response variable and the other features 
+	% as the input features
+
+	% TODO: Need to write function/code to extract training data from dataset
+	%		For now, we will just set the training and validation datasets to zeros
 	disp('****************************************')
 	disp('Getting training data...')
 	xtrain = double(zeros(2));
@@ -13,11 +20,13 @@ function [ networks ] = part1(x, y)
 	xval = double(zeros(2));
 	yval = double(zeros(2));
 
-	% initialize parameters for constructing network
+	% Initialize parameters for constructing network
+	numHiddenNodesPerLayer = 3;
 	[~, inputs] = size(xtrain);
 	numHiddenLayers = 1;
-	numHiddenNodesPerLayer = 3;
 	outputs = 1;
+
+	% Construct the architecture of the network
 	architecture = [inputs repmat(numHiddenNodesPerLayer, 1, numHiddenLayers) outputs];
 
 	% observe effects of different learning rates
