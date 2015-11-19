a522ae7f01b90617aad94e6acc3c385165fb5d75
Nick Layton <nllayton@ucdavis.edu>
2015-11-18 17:13:57 -0800

    continuing to create stubs for remaining parts

--- ANN/part4.m
+++ ANN/part4.m
@@ -6,8 +6,18 @@ function networks = part4( x, y )
 	disp('****************************************')
 	disp('*** PART 4 *****************************')
 
-    % IDEA: same as before, can we determine optimal learning rate?
     
+    % TODO: construct training and validation datasets from original
+	disp('****************************************')
+	disp('Getting training data...')
+	xtrain = double(zeros(2));
+	ytrain = double(zeros(2));
+	xval = double(zeros(2));
+	yval = double(zeros(2));
+    
+    
+    
+    % IDEA: same as before, can we determine optimal learning rate?
     
 	% initialize network parameters
     hiddenNodeValues = [3 6 9 12];
@@ -17,9 +27,12 @@ function networks = part4( x, y )
 	networks = [];
 
 
-	% train networks with different 
+	% train networks with different hidden layer/node values
 	for layerValue = hiddenLayerValues
 		for nodeValue = hiddenNodeValues
+            disp('****************************************')
+            disp(['Creating and Training with ' num2str(layerValue) ' layers and ' num2str(nodeValue) ' nodes...']);
+                
 			[~, inputs] = size(x);
 			architecture = [inputs repmat(nodeValue, 1, layerValue) outputs];
 			[network, L] = constructAndTrainNetwork(architecture, rate, xtrain, ytrain, xval, yval);
