f3898a941b47a3fa5ea61e48096aca8874e7ddb3
Nick Layton <nllayton@ucdavis.edu>
2015-11-18 17:13:57 -0800

    continuing to create stubs for remaining parts

---
+++ ANN/part5.m
@@ -0,0 +1,17 @@
+function prediction = part5( x, y )
+% part5(): performs calculations for part5 by using
+%          a Neural Network to predict the output
+	disp('****************************************')
+	disp('*** PART 5 *****************************')
+
+    % IDEA: should we try to use the most optimal learning rate and 
+    %       network configuration from earlier parts to make best
+    %       prediction?
+
+    % get trained network ...
+    
+    % make several predictions and verify their classification accuracy...
+
+    % display results (classification rate, error, etc...)
+end
+
