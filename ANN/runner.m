984aac4498fe70017380bdbf8c91e87d54457926
Nick Layton <nllayton@ucdavis.edu>
2015-11-18 17:43:15 -0800

    Refine the documentation for the runner and Part1

--- ANN/runner.m
+++ ANN/runner.m
@@ -1,7 +1,12 @@
-% runner: executes the functions for each part
+% runner: executes the functions for each part. The parts mirror the 
+%		  HW2 assignment problems
 
 
-% determine which problem the user wants to execute (simplifies testing)
+% Allow the user to decide which part they want to execute by reading 
+% input from the console. This will make testing/debugging easier than
+% having to execute all the parts at once. 
+% If the user inputs an invalid input type (e.g. puncuation marks and 
+% other symbols) it will continuously ask the user for a number (1 through 6)
 response = 'hi';
 while ~isnumeric(response)
 	disp('Which part would you like to run? Part 1, 2, 3, 4, 5, or 6');
@@ -18,38 +23,42 @@ while ~isnumeric(response)
 end
 
 
-% import the dataset
-% TODO: parse dataset
+% Import the dataset to be used within each part. 
+% The individual parts will determine if they want to divide
+% the dataset into smaller training and testing sets
+
+% TODO: need to implement a function to import the entire dataset.
+% for now, we will just set the dataset to all zeros
 x = zeros(100);
 y = zeros(100, 1);
 
 
-% perform first problem
+% Perform part 1 if user selected it as an option
 if (response == 1 || response == 0)
     part1(x, y);
 end
 
  
-% perform second problem
+% Perform part 2 if user selected it as an option
 if (response == 2 || response == 0)
 	part2(x, y);
 end
 
  
-% perform third problem
+% Perform part 3 if user selected it as an option
 if (response == 3 || response == 0)
 	disp('Part 3 actually has no implementation');
 end
 
  
-% perform fourth problem
+% Perform part 4 if user selected it as an option
 if (response == 4 || response == 0)
 	part4(x, y);
 end
  
-% perform fifth problem
-% if (response == 5 || response == 0)
-% 	part5(x, y);
-% end
+% Perform part 5 if user selected it as an option
+if (response == 5 || response == 0)
+	part5(x, y);
+end
  
-% perform sixth problem (should we be doing this?)
+% Perform part 6 if user selected it as an option (should we be doing this?)
