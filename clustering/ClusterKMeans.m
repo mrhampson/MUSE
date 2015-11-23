aab49bf84a8a60baa24f37251ff70ee391fa9da1
wotwell <hampson.mr@gmail.com>
2015-11-22 16:01:18 -0800

    Removed mode and year, get much better clusters

--- clustering/ClusterKMeans.m
+++ clustering/ClusterKMeans.m
@@ -2,37 +2,41 @@
 addpath('../Data');
 disp(sprintf('\nimporting dataset...'));
 data = dlmread('data-numeric-only.csv', ',', 2, 0);
-labels = {'duration', 'end of fade in', 'key', 'loudness', 'mode', 'song hotness', 'start of fade out', 'tempo', 'time signature', 'year'};
+labels = {'duration', 'end of fade in', 'key', 'loudness', 'song hotness', 'start of fade out', 'tempo', 'time signature'};
 
 %Take only the data we want and format as a matrix
 D = data(:, [2:3, 5:12]);
-D2 = D;
+D2 = D(:, [1:4, 6:9]);
 
 %normalize Data
-for j = 1:size(D,2)
-    D(:,j) = ( (D(:,j)-min(D(:,j)) )./(max(D(:,j))-min(D(:,j))));
+for j = 1:size(D2,2)
+    D2(:,j) = ( (D2(:,j)-min(D2(:,j)) )./(max(D2(:,j))-min(D2(:,j))));
 end
 
 %run k means
 disp(sprintf('Running 4 Cluster K-Means...'));
-[idx,C] = kmeans(D,4);
+[idx,C] = kmeans(D2,4);
 
 disp(sprintf('Plotting Data...'));
 
 %Plot The Clusters
-for i = 1:(size(D,2)-1)
+for i = 1:(size(D2,2))
     figure;
-    plot(D2(idx==1,i),D2(idx==1,6),'r.','MarkerSize',12)
+    plot(D2(idx==1,i),D2(idx==1,5),'r.','MarkerSize',12)
     hold on
-    plot(D2(idx==2,i),D2(idx==2,6),'b.','MarkerSize',12)
-    plot(D2(idx==3,i),D2(idx==3,6),'g.','MarkerSize',12)
-    plot(D2(idx==4,i),D2(idx==4,6),'y.','MarkerSize',12)
-    plot(C(:,i),C(:,6),'kx',...
+    plot(D2(idx==2,i),D2(idx==2,5),'b.','MarkerSize',12)
+    plot(D2(idx==3,i),D2(idx==3,5),'g.','MarkerSize',12)
+    plot(D2(idx==4,i),D2(idx==4,5),'y.','MarkerSize',12)
+    plot(C(:,i),C(:,5),'kx',...
          'MarkerSize',15,'LineWidth',3)
     legend('Cluster 1','Cluster 2','cluster 3','cluster 4','Centroids',...
            'Location','NW')
     xlabel(labels(i));
-    ylabel(labels(6));
+    ylabel(labels(5));
     title 'Cluster Assignments and Centroids'
     hold off
 end
+
+
+
+%K means on temp
--- clustering/heirarchalTest.m
+++ clustering/heirarchalTest.m
@@ -5,13 +5,7 @@ data = dlmread('data-numeric-only.csv', ',', 2, 0);
 labels = {'duration', 'end of fade in', 'key', 'loudness', 'mode', 'song hotness', 'start of fade out', 'tempo', 'time signature', 'year'};
 
 D = data(:, [2:3, 5:12]);
-D2 = D;
-
-[H,AX] = plotmatrix(D)
-for i = 1:length(AX)
-        ylabel(AX(i,1),labels{i})
-        xlabel(AX(end,i),labels{i})
-end
+D2 = D
 
 %normalize Data
 for j = 1:size(D,2)
@@ -37,3 +31,23 @@ C2 = cluster(L2, 'maxclust', 13);
 figure;
 dendrogram(L, 50);  %collapses all points into 50 leaf nodes
 %}
+
+
+%Plot The Clusters
+addpath('varycolor');
+colors = varycolor(20);
+for i = 2:(size(C,2)+1)
+    figure;
+    colors = varycolor(i);
+    plot(D2(C(:,i-1)==1,1),D2(C(:,i-1)==1,7),'.','MarkerSize',12, 'Color', colors(1,:))
+    hold on
+    for j = 2:i
+        plot(D2(C(:,i-1)==j,1),D2(C(:,i-1)==j,7),'.','MarkerSize',12, 'Color', colors(j,:))
+    end
+    legend('Cluster 1','Cluster 2','cluster 3','cluster 4','Centroids',...
+           'Location','NW')
+    xlabel(labels(i));
+    ylabel(labels(6));
+    title 'Cluster Assignments and Centroids'
+    hold off
+end
