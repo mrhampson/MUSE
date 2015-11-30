0881c8a96af22ee025bb89982193a9335ed5b532
Nick Layton <nllayton@ucdavis.edu>
2015-11-29 19:54:27 -0800

    Remove one of the ANN figures to condense report

--- ANN/part4.m
+++ ANN/part4.m
@@ -16,14 +16,14 @@ function optimalNetwork = part4( x, y )
     % These contain the optimal parameters
 
 
-    hiddenLayerValues = [3 4];
-	hiddenNodeValues = [9 12];
+    hiddenLayerValues = [4];
+	hiddenNodeValues = [12];
     % hiddenLayerValues = [4];
     % hiddenNodeValues = [12];
     [~, inputs] = size(x);
 	[~, outputs] = size(y);
 	networks = [];
-	rates = [0.05 0.1];
+	rates = [0.05];
     
     % These are the BAD parameters
  %    hiddenLayerValues = [3 4];
